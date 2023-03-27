ec64affb408b66b6ba52fef53ef645240afe46c9
pelzmagic <akinloyepelz@gmail.com>
2023-03-27 10:03:16 +0100

    update improvements

--- frontend/src/components/container/Container.jsx
+++ frontend/src/components/container/Container.jsx
@@ -1,90 +1,110 @@
-import { useState, useEffect } from 'react'
-import Task from '../task/Task'
+import { useState, useEffect } from "react"
+import Task from "../task/Task"
 import './container.css'
-import axios from 'axios'
 
 export default function Container() {
   const [lists, setLists] = useState([])
-  const backendUrl = 'http://localhost:3000'
+  const [refresh, setRefresh] = useState(false)
+  const backendUrl = 'http://localhost:3000';
   function handleAddList() {
-    setLists([
-      ...lists,
-      {
-        _id: `frontend${crypto.randomUUID()}`,
-        cards: [{ _id: crypto.randomUUID(), text: 'Add new task' }],
-      },
-    ])
+    let title = prompt('Enter list title: ')
+    if (title === '' || title === undefined || title === null) {
+      title = 'Untitled'
+    }
+    setLists([...lists, { _id: `frontend${crypto.randomUUID()}`, title: title, cards: [{ _id: `frontend${crypto.randomUUID()}`, text: 'Add new task' }] }])
+    saveTitle(title) // Save list title to database
   }
 
+  // Function to save list title to database
+  function saveTitle(title='') {
+    fetch(`${backendUrl}/api/v1/list/`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+      },
+      body: JSON.stringify({
+          title: title,
+      })
+    })
+    .then(res => {
+      if (res.status === 200) {
+          setRefresh(true)
+          console.log('Title saved')
+          return res.json()
+      } else {
+          throw new Error('Error saving list')
+      }
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }
+
+  // useFfect to get lists from database when refresh is true
   useEffect(() => {
-    const fetchList = async () => {
-      const config = {
+    if (refresh === true) {
+      fetch(`${backendUrl}/api/v1/list/`, {
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${JSON.parse(
-            sessionStorage.getItem('token')
-          )}`,
-        },
-      }
-
-      try {
-        const res = await axios.get(`${backendUrl}/api/v1/list/`, { ...config })
-        setLists(res.data.list)
-      } catch (error) {
-        console.log(error)
-      }
+          'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+        }
+      })
+      .then(res => {
+        if (res.status === 200) {
+          setRefresh(false)
+          return res.json()
+        } else {
+          throw new Error('Error getting lists')
+        }
+      })
+      .then(data => {
+        console.log(data)
+        setLists(data.list)
+      })
+      .catch(err => console.log(err))
     }
+  }, [refresh])
 
-    fetchList()
-    // fetch(`${backendUrl}/api/v1/list/`, {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
-    //   },
-    // })
-    //   .then((res) => {
-    //     if (res.status === 200) {
-    //       console.log(res.json())
-    //       return res.json()
-    //     } else {
-    //       throw new Error('Error getting lists')
-    //     }
-    //   })
-    //   .then((data) => {
-    //     console.log(data)
-    //     setLists(data.list)
-    //   })
-    //   .catch((err) => console.log(err))
+  // useEffect to get lists from database when component mounts
+  useEffect(() => {
+    fetch(`${backendUrl}/api/v1/list/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        } else {
+          throw new Error('Error getting lists')
+        }
+      })
+      .then(data => {
+        console.log(data)
+        setLists(data.list)
+      })
+      .catch(err => console.log(err))
   }, [])
 
   return (
-    <main className='main-container'>
-      <div className='add-list' onClick={handleAddList}>
-        New
-      </div>
-      <div className='main-content'>
-        {lists.map((list) => {
-          return (
-            <Task
-              key={list._id}
-              title={list.title}
-              cardsList={
-                list.cards.length > 0
-                  ? list.cards
-                  : [
-                      {
-                        _id: `frontend${crypto.randomUUID()}`,
-                        text: 'Add new task',
-                      },
-                    ]
-              }
-              listId={list._id}
-              setLists={setLists}
-            />
-          )
-        })}
-      </div>
+    <main className="main-container">
+        <div className="add-list" onClick={handleAddList}>
+            Add List
+        </div>
+        <div className="main-content">
+          {lists.map(list => {
+            return <Task
+                      key={list._id}
+                      title={list.title}
+                      cardsList={ list.cards.length > 0 ? list.cards : [{ _id: `frontend${crypto.randomUUID()}`, text: 'Add new task' }] }
+                      listId={list._id}
+                      setLists={setLists}
+                    />
+          })}
+        </div>
     </main>
   )
 }
--- frontend/src/components/task/Card.jsx
+++ frontend/src/components/task/Card.jsx
@@ -1,203 +1,148 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import './card.css'
 
-export default function Card({
-  content,
-  setCards,
-  cardId,
-  listId,
-  color = '',
-  setLists,
-  title,
-}) {
-  const [text, setText] = useState(content)
-  const [refresh, setRefresh] = useState(false)
-  const [showTrash, setShowTrash] = useState(false)
-  const backendUrl = 'http://localhost:3000'
-  function handleShowTrash() {
-    setShowTrash(!showTrash)
-  }
-
-  useEffect(() => {
-    if (refresh === true) {
-      fetch(`${backendUrl}/api/v1/list/`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${JSON.parse(
-            sessionStorage.getItem('token')
-          )}`,
-        },
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            setRefresh(false)
-            return res.json()
-          } else {
-            throw new Error('Error getting lists')
-          }
-        })
-        .then((data) => {
-          console.log(data)
-          setCards(data.list.filter((item) => item._id === listId)[0].cards)
-        })
-        .catch((err) => console.log(err))
+export default function Card({ content, setCards, cardId, listId, color='', title }) {
+    const [text, setText] = useState(content);
+    const [refresh, setRefresh] = useState(false)
+    const [showTrash, setShowTrash] = useState(false);
+    const backendUrl = 'http://localhost:3000';
+    function handleShowTrash() {
+        setShowTrash(!showTrash)
     }
-  }, [refresh])
 
-  function handleContentChange(event) {
-    setText(event.target.value)
-    setCards((prev) => {
-      return prev.map((item) => {
-        if (item.id === cardId) {
-          console.log(item)
-          return { ...item, text: event.target.value }
+    useEffect(() => {
+        if (refresh === true) {
+            fetch(`${backendUrl}/api/v1/list/`, {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+                }
+              })
+                .then(res => {
+                  if (res.status === 200) {
+                    setRefresh(false)
+                    return res.json()
+                  } else {
+                    throw new Error('Error getting lists')
+                  }
+                })
+                .then(data => {
+                  console.log(data)
+                  setCards(data.list.filter((item) => item._id === listId)[0].cards)
+                })
+                .catch(err => console.log(err))
         }
-        return item
-      })
-    })
-  }
-  const handleDelete = async (id) => {
-    if (id.slice(0, 8) === 'frontend') {
-      setCards((prev) => prev.filter((item) => item._id !== id))
-      alert('Card deleted')
-      return
-    }
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
-      },
+    }, [refresh])
+
+    function handleContentChange(event) {
+        setText(event.target.value);
+        setCards((prev) => {
+            return prev.map((item) => {
+                if (item.id === cardId) {
+                    console.log(item)
+                    return { ...item, text: event.target.value }
+                }
+                return item
+            })
+        })
     }
 
-    try {
-      const res = await axios.delete(`${backendUrl}/api/v1/card/${id}`, {
-        ...config,
-      })
-      if (res.status == 200) {
-        setRefresh(true)
-        alert('Card saved')
-        console.log(res.data)
-      }
-    } catch (error) {
-      console.log(error)
+    function handleDelete(id) {
+        console.log(id)
+        if (id.slice(0, 8) === 'frontend') {
+            setCards((prev) => prev.filter((item) => item._id !== id))
+            alert('Card deleted');
+            return
+        }
+        fetch(`${backendUrl}/api/v1/card/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+            },
+            body: JSON.stringify({
+                listId,
+            })
+        })
+        .then(res => {
+            if (res.status === 200) {
+                alert('Card deleted');
+                return res.json()
+            } else {
+                throw new Error('Error deleting card')
+            }
+        })
+        .then(data => {
+            console.log(data)
+            setCards((prev) => {
+                return prev.filter((item) => item._id !== id)
+            })
+        })
+        .catch(err => console.log(err))
     }
-  }
-  // function handleDelete(id) {
-  //     console.log(id)
-  //     if (id.slice(0, 8) === 'frontend') {
-  //         setCards((prev) => prev.filter((item) => item._id !== id))
-  //         alert('Card deleted');
-  //         return
-  //     }
-  //     fetch(`${backendUrl}/api/v1/card/${id}`, {
-  //         method: 'DELETE',
-  //         headers: {
-  //             'Content-Type': 'application/json',
-  //             'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
-  //         },
-  //         body: JSON.stringify({
-  //             listId,
-  //         })
-  //     })
-  //     .then(res => {
-  //         if (res.status === 200) {
-  //             alert('Card deleted');
-  //             return res.json()
-  //         } else {
-  //             throw new Error('Error deleting card')
-  //         }
-  //     })
-  //     .then(data => {
-  //         console.log(data)
-  //         setCards((prev) => {
-  //             return prev.filter((item) => item._id !== id)
-  //         })
-  //     })
-  //     .catch(err => console.log(err))
-  // }
 
-  function handleSaveCard(cardId, listId) {
-    console.log(cardId, listId)
-    if (title === '') {
-      alert('Please enter a title for the list')
-      return
+    function handleSaveCard(cardId, listId) {
+        if (title === '' || title === undefined) {
+            alert('Please enter a title for the list');
+            return
+        }
+        if (cardId.slice(0, 8) === 'frontend') {
+            fetch(`${backendUrl}/api/v1/card/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+                },
+                body: JSON.stringify({
+                    listId,
+                    text,
+                })
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    setRefresh(true)
+                    alert('Card saved');
+                    return res.json()
+                } else {
+                    alert('Check if you have entered a title for the list')
+                    throw new Error('Error saving card')
+                }
+            })
+            .then(data => {
+             console.log(data)
+            })
+            .catch(err => console.log(err))
+        } else {
+            fetch(`${backendUrl}/api/v1/card/${cardId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+                },
+                body: JSON.stringify({
+                    text
+                })
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    alert('Card updated');
+                    return res.json()
+                } else {
+                    throw new Error('Error updating card')
+                }
+            })
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+        }
     }
-    if (cardId.slice(0, 8) === 'frontend') {
-      fetch(`${backendUrl}/api/v1/card/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${JSON.parse(
-            sessionStorage.getItem('token')
-          )}`,
-        },
-        body: JSON.stringify({
-          listId,
-          text,
-        }),
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            setRefresh(true)
-            alert('Card saved')
-            return res.json()
-          } else {
-            throw new Error('Error saving card')
-          }
-        })
-        .then((data) => {
-          console.log(data)
-        })
-        .catch((err) => console.log(err))
-    } else {
-      fetch(`${backendUrl}/api/v1/card/${cardId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${JSON.parse(
-            sessionStorage.getItem('token')
-          )}`,
-        },
-        body: JSON.stringify({
-          text,
-        }),
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            alert('Card updated')
-            return res.json()
-          } else {
-            throw new Error('Error updating card')
-          }
-        })
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
-    }
-  }
-
+    
   return (
-    <div
-      className={`card ${color}`}
-      onMouseEnter={handleShowTrash}
-      onMouseLeave={handleShowTrash}
-    >
-      <i
-        className={showTrash ? 'bx bxs-trash show-trash' : 'bx bxs-trash'}
-        onClick={() => handleDelete(cardId)}
-      ></i>
-      <textarea
-        className='box-textarea'
-        value={text}
-        onChange={handleContentChange}
-      ></textarea>
-      <button
-        className='save-btn'
-        onClick={() => handleSaveCard(cardId, listId)}
-      >
-        Save
-      </button>
+    <div className={`card ${color}`} onMouseEnter={handleShowTrash} onMouseLeave={handleShowTrash} >
+        <i className={ showTrash ? 'bx bxs-trash show-trash' : 'bx bxs-trash' } onClick={() => handleDelete(cardId)}></i>
+        <textarea className='box-textarea' value={text} onChange={handleContentChange} >
+        </textarea>
+        <button className='save-btn' onClick={() => handleSaveCard(cardId, listId)}>Save</button>
     </div>
   )
 }
